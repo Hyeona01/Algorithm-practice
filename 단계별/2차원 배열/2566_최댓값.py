77228529e19590ae1339d08e9f10257aa58ba25d
Hyeona01 <sha0209@dgu.ac.kr>
2024-02-09 23:38:13 +0900

    0209 백준 2745번_진법변환

--- 단계별/2차원 배열/2566_최댓값.py
+++ 단계별/2차원 배열/2566_최댓값.py
@@ -5,7 +5,8 @@ for i in range(1, 10):
   row = list(map(int,input().split()))
   if max(row) > max_num :
     max_num = max(row)
-    row = i
-    col = row.index(str(max_num))
+    row = i + 1
+    col = row.index(max_num) + 1
 
-print(max_num, row, col)
+print(max_num)
+print(row, col)
