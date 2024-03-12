9b75b79a16a90a5ec4d5bdad46c016ecda7d1e37
Hyeona01 <sha0209@dgu.ac.kr>
2024-03-12 18:15:11 +0900

    0312 그리디/큰수의법칙 오답노트

--- 그리디/큰 수의 법칙/풀이.py
+++ 그리디/큰 수의 법칙/풀이.py
@@ -1,8 +1,12 @@
+import time
+
 n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
 result = 0
 
 nums.sort()
+
+start_time = time.time() # 시간 복잡도 측정
 
 while m > 0 :
   for _ in range(k):
@@ -18,4 +22,9 @@ while m > 0 :
   else :
     break
 
+end_time = time.time() # 시간 복잡도 측정
+
 print(result)
+print(end_time - start_time)
+
+# 소요시간 : 2.9
