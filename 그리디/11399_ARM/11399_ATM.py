7442207004bde386e70f57f92a131f48d0c377c3
Hyeona01 <sha0209@dgu.ac.kr>
2024-03-18 15:44:27 +0900

    0318 백준 11399번 ATM/성공

--- 그리디/11399_ARM/11399_ATM.py
+++ 그리디/11399_ARM/11399_ATM.py
@@ -4,8 +4,8 @@ time_list = list(map(int, input().split()))
 time_list.sort()
 
 result = 0
-for time in time_list:
-  result += result
-  result += time
+for i in range(0, len(time_list)):
+  for j in range(0, i+1):
+    result += time_list[j]
 
 print(result)
