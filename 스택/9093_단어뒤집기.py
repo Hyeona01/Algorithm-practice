08943fae44634c908207efe22a2bd1bde1d58b52
Hyeona01 <sha0209@dgu.ac.kr>
2024-05-15 23:14:58 +0900

    0515 백준 9093번 단어뒤집기 - 성공

--- 스택/9093_단어뒤집기.py
+++ 스택/9093_단어뒤집기.py
@@ -1,10 +1,11 @@
 T = int(input())
 lst = []
+lst_reverse = [T]
 
 for i in range(T):
+  temp = ""
   lst = list(map(str,input().split()))
   
   for j in range(len(lst)):
-    print(lst[i][-1])
-
-# print(lst)
+    temp += lst[j][::-1] + " "
+  print(temp)
